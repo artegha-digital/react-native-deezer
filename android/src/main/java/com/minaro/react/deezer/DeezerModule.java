8f59de7ba7c5e444bcf8e8d4b9f2819ecbff9ac5
Minaro <45274627+MinaroShikuchi@users.noreply.github.com>
2019-09-09 02:28:41 +0200

    1.0.18: authenticate popup

--- android/src/main/java/com/minaro/react/deezer/DeezerModule.java
+++ android/src/main/java/com/minaro/react/deezer/DeezerModule.java
@@ -1,9 +1,18 @@
 package com.minaro.react.deezer;
 
+import android.os.Bundle;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+
+import com.deezer.sdk.network.connect.DeezerConnect;
+import com.deezer.sdk.network.connect.event.DialogListener;
+
+import com.deezer.sdk.model.Permissions;
+
 
 public class DeezerModule extends ReactContextBaseJavaModule {
 
@@ -23,5 +32,42 @@ public class DeezerModule extends ReactContextBaseJavaModule {
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @ReactMethod
+    public void getModuleName(Callback callback) {
+        callback.invoke("Hello Deezer");
+    }
+
+    @ReactMethod
+    public void authenticate(final Promise promise) {
+        String applicationID = "364784";
+        DeezerConnect deezerConnect = new DeezerConnect(applicationID);
+
+        // the request listener
+        String[] permissions = new String[] {
+                Permissions.BASIC_ACCESS,
+                Permissions.MANAGE_LIBRARY,
+                Permissions.LISTENING_HISTORY
+        };
+
+        // The listener for authentication events
+        DialogListener listener = new DialogListener() {
+
+            public void onComplete(Bundle values) {
+                promise.resolve(true);
+            }
+
+            public void onCancel() {
+                promise.resolve(false);
+            }
+
+            public void onException(Exception e) {
+                promise.resolve(false);
+            }
+        };
+
+        // Launches the authentication process
+        deezerConnect.authorize(this.getCurrentActivity(), permissions, listener);
     }
 }
